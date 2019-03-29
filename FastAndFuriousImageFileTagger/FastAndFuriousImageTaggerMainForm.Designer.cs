b511244d6846e8bc7e8a6ced3f8c0771aafec136
Tom Trottel <tom.trottel.70@outlook.de>
2019-03-29 17:58:12 +0100

    the image in the picturebox now preservers its aspect ratio

--- FastAndFuriousImageFileTagger/FastAndFuriousImageTaggerMainForm.Designer.cs
+++ FastAndFuriousImageFileTagger/FastAndFuriousImageTaggerMainForm.Designer.cs
@@ -86,7 +86,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(16, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(315, 309);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
